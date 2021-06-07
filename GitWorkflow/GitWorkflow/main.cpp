4c3a0fa71aeb38728b1767967c819ef65453dd16
DESKTOP-3BHDUSP\Samuel <samuelasp96@gmail.com>
2021-06-07 18:15:40 +0200

    Create and render bottom platform.
    Platform is hard coded to the lower 10% of the screen.
    After window commit is merged, this size will be based on window size.

--- GitWorkflow/GitWorkflow/main.cpp
+++ GitWorkflow/GitWorkflow/main.cpp
@@ -1,11 +1,22 @@
 #include <SFML/Graphics.hpp>
 
 #include "Player.hpp"
+#include "Platform.hpp"
+
+Platform createBottomPlatform()
+{
+	Platform platform;
+	platform.setPos(0.0f, 190.0f);
+	platform.setSize(200.0f, 10.0f);
+	platform.setColor(sf::Color(0ui8, 150ui8, 0ui8));
+	return platform;
+}
 
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
 	Player player;
+	Platform bottomPlatform = createBottomPlatform();
 
 	while (window.isOpen())
 	{
@@ -17,6 +28,7 @@ int main()
 		}
 
 		window.clear();
+		window.draw(bottomPlatform);
 		window.draw(player);
 		window.display();
 	}
