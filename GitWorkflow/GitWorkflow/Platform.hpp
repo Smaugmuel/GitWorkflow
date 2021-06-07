78f80f5666349ca781151d36c43523a5acd7c187
DESKTOP-3BHDUSP\Samuel <samuelasp96@gmail.com>
2021-06-07 18:07:44 +0200

    Fix merge conflicts

--- GitWorkflow/GitWorkflow/Platform.hpp
+++ GitWorkflow/GitWorkflow/Platform.hpp
@@ -1,14 +1,23 @@
 #pragma once
 #include <SFML/Graphics/RectangleShape.hpp>
+#include <SFML/Graphics/RenderTarget.hpp>
 
-class Platform final
+class Platform final : sf::Drawable
 {
 public:
 	Platform() = default;
 	~Platform() = default;
 
 	void setPos(float x, float y) { rect_.setPosition(x, y); }
+	void setWidth(float x, float y) { rect_.setSize({ x, y }); }
+	void setColor(sf::Color color) { rect_.setFillColor(color); }
+
+	sf::Vector2f getPos() const { return rect_.getPosition(); }
+	sf::Vector2f getWidth() const { return rect_.getSize(); }
+	sf::Color getColor() const { return rect_.getFillColor(); }
 
 private:
+	void draw(sf::RenderTarget& target, sf::RenderStates states) { target.draw(rect_, states); }
+
 	sf::RectangleShape rect_;
 };
