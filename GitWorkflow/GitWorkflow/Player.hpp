7b3dc07c92ca01d3b066873ab4e06d9b001c82e0
ChristopherKallstrom <christopher.kallstrom@gmail.com>
2021-06-07 17:50:21 +0200

    Changed variable names to be more clear

--- GitWorkflow/GitWorkflow/Player.hpp
+++ GitWorkflow/GitWorkflow/Player.hpp
@@ -6,18 +6,18 @@ class Player final : public sf::Drawable
 {
 public:
 	Player(const sf::Vector2f& size = sf::Vector2f(100.f, 100.f), const sf::Color& color = sf::Color::Red)
-		: m_shape(size)
+		: m_model(size)
 	{
-		m_shape.setFillColor(color);
+		m_model.setFillColor(color);
 	}
 
 	~Player() = default;
 
 private:
-	sf::RectangleShape m_shape;
+	sf::RectangleShape m_model;
 
 	void draw(sf::RenderTarget& target, sf::RenderStates states) const override 
 	{
-		target.draw(m_shape, states);
+		target.draw(m_model, states);
 	}
 };
