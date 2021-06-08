44f48ed822a73527d1aafcf697e3d60eb5b6f5a0
ChristopherKallstrom <christopher.kallstrom@gmail.com>
2021-06-08 18:29:39 +0200

    Add end point the player can intersect with to exit program

--- GitWorkflow/GitWorkflow/main.cpp
+++ GitWorkflow/GitWorkflow/main.cpp
@@ -7,6 +7,11 @@
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "SFML works!");
+	sf::RectangleShape endPoint({ 100.f, 100.f });
+
+	endPoint.setPosition({ 300.f, 0.f });
+	endPoint.setFillColor(sf::Color::Green);
+
 	Player player;
 
 	while (window.isOpen())
@@ -20,7 +25,10 @@ int main()
 
 		window.clear();
 		player.update();
+		if (player.getGlobalBounds().intersects(endPoint.getGlobalBounds())) { window.close(); }
+
 		window.draw(player);
+		window.draw(endPoint);
 		window.display();
 	}
 
