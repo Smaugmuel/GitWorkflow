0a56a9d47562cf01b12001e74dd6ee0b76f4b276
ChristopherKallstrom <christopher.kallstrom@gmail.com>
2021-06-11 14:09:58 +0200

    Add jump and collision support, change declarations to be seperate from definitions

--- GitWorkflow/GitWorkflow/Player.hpp
+++ GitWorkflow/GitWorkflow/Player.hpp
@@ -1,23 +1,33 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
+#include "Platform.hpp"
+#include "Utility.hpp"
 
 class Player final : public sf::Drawable
 {
 public:
-	Player(const sf::Vector2f& size = sf::Vector2f(100.f, 100.f), const sf::Color& color = sf::Color::Red)
-		: m_model(size)
-	{
-		m_model.setFillColor(color);
-	}
-
+	Player(const sf::Vector2f& position = { 0.0f, 0.0f }, const sf::Vector2f& size = { 100.0f, 100.0f }, const sf::Color& color = sf::Color::Red);
 	~Player() = default;
 
-private:
-	sf::RectangleShape m_model;
+	void move(const sf::Vector2f& offset);
+	void setPosition(const sf::Vector2f& position);
+	const sf::Vector2f& getPosition() const;
 
-	void draw(sf::RenderTarget& target, sf::RenderStates states) const override 
-	{
-		target.draw(m_model, states);
-	}
+	void setVelocity(const sf::Vector2f& velocity);
+	const sf::Vector2f& getVelocity() const;
+
+	const sf::FloatRect getGlobalBounds() const;
+	void collide(const Platform& platform);
+
+	void update(const float dt = 0.0f);
+
+private:
+	void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+
+	sf::RectangleShape m_model;
+	sf::Vector2f m_velocity = { 0.0f, 0.0f };
+
+	float m_speed = 40.0f;
+	bool m_hasJumped = true;
 };
