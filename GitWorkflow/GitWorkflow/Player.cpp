61c1759fb01652b571c2a1a9ad4c241760af87c5
ChristopherKallstrom <christopher.kallstrom@gmail.com>
2021-06-13 23:53:13 +0200

    Add reduced jump height if not holding space after jumping

--- GitWorkflow/GitWorkflow/Player.cpp
+++ GitWorkflow/GitWorkflow/Player.cpp
@@ -30,11 +30,16 @@ void Player::update(const float dt)
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) { if (std::abs(m_velocity.x) < m_speed * (std::roundf(m_velocity.y) == 0.0f ? 1.0f : 0.5f)) m_velocity.x += m_acceleration * dt * (std::roundf(m_velocity.y) == 0.0f ? 1.0f : 0.5f); }
 	else if (m_velocity.x > 0.0f) m_velocity.x -= m_acceleration * dt;
 
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space) && !m_hasJumped)
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
 	{
-		m_velocity.y -= m_speed;
-		m_hasJumped = true;
+		if (!m_hasJumped)
+		{
+			m_velocity.y -= m_speed;
+			m_hasJumped = true;
+		}
 	}
+
+	else if (m_hasJumped) { m_velocity.y += m_speed * dt;  }
 
 	move(sf::Vector2f(0.0f, 9.82f) * (dt * dt / 2.0f));
 	m_velocity.y += 9.82f * 4.0f * dt; // * 4 IS FOR TESTING PURPOSES ONLY
